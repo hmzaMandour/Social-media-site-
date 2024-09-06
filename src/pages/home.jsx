718801c6559cf1886927f8b0969d661d4732d1bd
Adamelbouchiri <matrakmtk@gmail.com>
2024-09-06 12:23:48 +0200

    solve-check-problem-and-finish-checking-first-step

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,5 @@
-import { AboutSocial } from "./aboutSocial/aboutSocial";
-
 import React from "react";
+import { AboutSocial } from "./aboutSocial/aboutSocial";
 import { Routes, Route } from "react-router-dom";
 import { Signup } from "./signup/signup";
 import { SignIn } from "./signin/signin";
@@ -9,15 +8,16 @@ import { Navbar } from "../layouts/navBar";
 import { Creators } from "./creators/creators";
 import UserProfile from "./profile/userprofile";
 import UsersProfile from "./profile/usersprofile";
+
 export const Home = () => {
   return (
     <>
       <Routes>
-      <Route path="/navbar" element={<Navbar />} />
-      <Route path="/sidebar" element={<Side />} />
-      <Route path="/" element={<AboutSocial />}></Route>
-        <Route path="/profile" element={<UsersProfile/>}></Route>
-        <Route path="/user" element={<UserProfile/>}></Route>
+        <Route path="/" element={<AboutSocial />}></Route>
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/sidebar" element={<Side />} />
+        <Route path="/profile" element={<UsersProfile />}></Route>
+        <Route path="/user" element={<UserProfile />}></Route>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/creators" element={<Creators />} />
