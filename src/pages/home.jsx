34cdadd511cc038275398ff7acafa50a7c09fb06
Adamelbouchiri <matrakmtk@gmail.com>
2024-09-09 10:41:43 +0200

    change-to-useState-object-and-add-edits-to-signIn/Up-and-set-friends-array

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext } from "react";
 import { AboutSocial } from "./aboutSocial/aboutSocial";
 import { Routes, Route } from "react-router-dom";
 import { Signup } from "./signup/signup";
@@ -10,26 +10,32 @@ import UserProfile from "./profile/userprofile";
 import UsersProfile from "./profile/usersprofile";
 import { Homepage } from "./home/home";
 import { Stories } from "./home/stories";
-
 import { Friend } from "./friends/friends";
 
+export const friendsArr = createContext(null);
+
 export const Home = () => {
+
+  let friends = []
+
   return (
     <>
-      <Routes>
-        <Route path="/profile" element={<UsersProfile />}></Route>
-       <Route path="/home" element={<Homepage />} />
-       <Route path="/storie" element={<Stories />} />
-        <Route path="/user" element={<UserProfile />}></Route>
-        <Route path="/navbar" element={<Navbar />} />
-        <Route path="/sidebar" element={<Side />} />
-        <Route path="/" element={<AboutSocial />}></Route>
-        <Route path="/:id" element={<UsersProfile />}></Route>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/friend" element={<Friend />} />
-        <Route path="/creators" element={<Creators />} />
-      </Routes>
+      <friendsArr.Provider value={friends}>
+        <Routes>
+          <Route path="/profile" element={<UsersProfile />}></Route>
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/storie" element={<Stories />} />
+          <Route path="/user" element={<UserProfile />}></Route>
+          <Route path="/navbar" element={<Navbar />} />
+          <Route path="/sidebar" element={<Side />} />
+          <Route path="/" element={<AboutSocial />}></Route>
+          <Route path="/:id" element={<UsersProfile />}></Route>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/friend" element={<Friend />} />
+          <Route path="/creators" element={<Creators />} />
+        </Routes>
+      </friendsArr.Provider>
     </>
   );
 };
